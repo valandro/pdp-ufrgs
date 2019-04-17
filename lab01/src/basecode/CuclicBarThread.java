9d49d9463f11626b136f3fec672a8bb206208a03
Lucas <lvalandrorocha@hotmail.com>
2019-04-16 21:12:14 -0300

    [INFO-LAB]

--- lab01/src/basecode/CuclicBarThread.java
+++ lab01/src/basecode/CuclicBarThread.java
@@ -17,16 +17,18 @@ public class CuclicBarThread implements Runnable {
 		    Thread.sleep(10);
                 }
                 System.out.println(Thread.currentThread().getName() + " :: Barreira 1: Esperando o primeiro estágio ser completado");
-
+                barrier.await();
                 for (int i = 1; i < 5; i++) {
                     Thread.sleep(200);
                 }
                 System.out.println(Thread.currentThread().getName() + " :: Barreira 2: Esperando o segundo estágio ser completado");
+                barrier.await();
 
                 for (int i = 1; i < 5; i++) {
 		    Thread.sleep(10);
                 }
                 System.out.println(Thread.currentThread().getName() + " :: Barreira 3: Esperando o terceiro estágio ser completado");
+                barrier.await();
 
                 System.out.println(Thread.currentThread().getName()+" :: Thread encerrada");
             } catch (Exception ex) {
